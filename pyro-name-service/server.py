0cecb7dbb6b0940aaa896404475915f398d46bd1
Breno-Silva1 <bbreno100@live.com>
2018-10-11 09:57:31 -0300

    soap java

--- pyro-name-service/server.py
+++ pyro-name-service/server.py
@@ -1,20 +1,20 @@
 import Pyro.core
+import Pyro.naming
 
 class JokeGen(Pyro.core.ObjBase):
-		def __init__(self):
+        def __init__(self):
                 Pyro.core.ObjBase.__init__(self)
         def joke(self, name):
                 return "Sorry "+name+", I don't know any jokes."
 
 def main():
-	Pyro.core.initServer()
+    Pyro.core.initServer()
+    ns=Pyro.naming.NameServerLocator().getNS()
     daemon=Pyro.core.Daemon()
+    daemon.useNameServer(ns)
     uri=daemon.connect(JokeGen(),"jokegen")
-
-    print "The daemon runs on port:",daemon.port
-    print "The object's uri is:",uri
-
     daemon.requestLoop()
-    
+
 if __name__=="__main__":
     main()
+
